1798eeb8a50f65e7a80884145058b0ab463252da
kesiye iki <123519408+kemido2023@users.noreply.github.com>
2023-07-04 11:05:03 +0100

    Update deploy.py
    update by kca

--- deploy.py
+++ deploy.py
@@ -4,4 +4,7 @@ echo "Hello world"
 #shell script
 python ('Hello world')
 #python script
-
+echo "Thanks for adding me up" 
+whoami
+echo "we are on our way to greater heights in the IT world"
+echo "cheers"
